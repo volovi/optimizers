3a7e8b240741915fd69ba86420ef89fac50cc61c
Alexei Volovikov <alexei.volovikov@gmail.com>
2025-01-25 13:16:21 +0200

    Minor code refactoring

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.animation as animation
 learning_rate = 0.02
 beta = 0.95
 epsilon = 1e-8
-epochs = 100
+num = 100
 start_point = (0.25, 0.45)
 
 
@@ -18,92 +18,86 @@ def dcost(x, y):
 
 
 def gradient_descent():
-    k, p = np.array(start_point), np.zeros((3, epochs))
+    p, w = create_path('GD'), np.array(start_point)
 
-    r = p[:, 0] = *k, cost(*k)
-    i = 1
+    while p(w):
+        w -= learning_rate * dcost(*w)
 
-    while r[-1] > epsilon and i < epochs:
-        k -= learning_rate * dcost(*k)
-        r = p[:, i] = *k, cost(*k)
-        i += 1
-
-    return 'GD', *p
+    return p.get_data()
 
 
 def momentum_optimizer():
-    k, p = np.array(start_point), np.zeros((3, epochs))
-    v = np.zeros(2)
+    p, w = create_path('Momentum'), np.array(start_point)
+    m = np.zeros(2)
 
-    r = p[:, 0] = *k, cost(*k)
-    i = 1
+    while p(w):
+        m = beta * m + (1 - beta) * learning_rate * dcost(*w)
+        w -= m
 
-    while r[-1] > epsilon and i < epochs:
-        v = beta * v + learning_rate * (1 - beta) * dcost(*k)
-        k -= v
-        r = p[:, i] = *k, cost(*k)
-        i += 1
-
-    return 'Momentum', *p
+    return p.get_data()
 
 
 def nesterov_optimizer():
-    k, p = np.array(start_point), np.zeros((3, epochs))
-    v = np.zeros(2)
+    p, w = create_path('Nesterov'), np.array(start_point)
+    m = np.zeros(2)
 
-    r = p[:, 0] = *k, cost(*k)
-    i = 1
+    while p(w):
+        m = beta * m + (1 - beta) * learning_rate * dcost(*(w - beta * m))
+        w -= m
 
-    while r[-1] > epsilon and i < epochs:
-        v = beta * v + learning_rate * (1 - beta) * dcost(*(k - beta * v))
-        k -= v
-        r = p[:, i] = *k, cost(*k)
-        i += 1
-
-    return 'Nesterov', *p
+    return p.get_data()
 
 
 def rmsprop_optimizer():
-    k, p = np.array(start_point), np.zeros((3, epochs)) 
+    p, w = create_path('RMSProp with Momentum'), np.array(start_point)
     m, v = np.zeros(2), np.zeros(2)
 
-    r = p[:, 0] = *k, cost(*k)
-    i = 1
+    while p(w):
+        dw = dcost(*w)
+        v = beta * v + (1 - beta) * dw ** 2
+        m = beta * m + (1 - beta) * learning_rate / (np.sqrt(v) + epsilon) * dw
+        w -= m
 
-    while r[-1] > epsilon and i < epochs:
-        dk = dcost(*k)
-        v = beta * v + (1 - beta) * dk ** 2
-        m = beta * m + (1 - beta) * learning_rate / (np.sqrt(v) + epsilon) * dk
-        k -= m
-        r = p[:, i] = *k, cost(*k)
-        i += 1
-
-    return 'RMSProp with Momentum', *p
+    return p.get_data()
 
 
 def adam_optimizer():
-    k, p = np.array(start_point), np.zeros((3, epochs))
+    p, w = create_path('Adam'), np.array(start_point)
     m, v = np.zeros(2), np.zeros(2)
 
-    r = p[:, 0] = *k, cost(*k)
-    i = 1
+    while p(w):
+        dw = dcost(*w)
+        m = beta * m + (1 - beta) * dw
+        v = beta * v + (1 - beta) * dw ** 2
+        m_hat = m / (1 - beta ** p.get_i())
+        v_hat = v / (1 - beta ** p.get_i())
+        w -= learning_rate / (np.sqrt(v_hat) + epsilon) * m_hat
 
-    while r[-1] > epsilon and i < epochs:
-        dk = dcost(*k)
-        m = beta * m + (1 - beta) * dk
-        v = beta * v + (1 - beta) * dk ** 2
-        m_hat = m / (1 - beta ** i)
-        v_hat = v / (1 - beta ** i)
-        k -= learning_rate / (np.sqrt(v_hat) + epsilon) * m_hat
-        r = p[:, i] = *k, cost(*k)
-        i += 1
+    return p.get_data()
 
-    return 'Adam', *p
+
+def create_path(label):
+    p = np.zeros((3, num))
+    i = 0
+
+    def inner(k):
+        nonlocal p, i
+        z = cost(*k)
+        r = z > epsilon and i < num
+        if r:
+            p[:, i] = *k, z
+            i += 1
+        return r
+
+    inner.get_data = lambda: (label, *p)
+    inner.get_i = lambda: i
+
+    return inner
 
 
 def update(i):
+    j, k = slice(i, i + 1), slice(i + 1)
     for x, y, z, l1, l2, l3 in zip(X, Y, Z, lines1, lines2, lines3):
-        j, k = slice(i, i + 1), slice(i + 1)
         l1.set_data(range(i + 1), z[k])
         l2.set_data(x[j], y[j])
         l3.set_data_3d(x[j], y[j], z[j])
@@ -111,7 +105,7 @@ def update(i):
     return (*lines1, *lines2, *lines3)
 
 
-fig = plt.figure(figsize=(10, 6), layout="constrained")
+fig = plt.figure(figsize=(10, 6), layout='constrained')
 
 ax1 = fig.add_subplot(2, 3, 1)
 ax2 = fig.add_subplot(2, 3, 4)
@@ -125,10 +119,9 @@ ax3.plot_surface(X, Y, Z, alpha=0.1, cmap='CMRmap')
 
 labels, X, Y, Z = zip(gradient_descent(), momentum_optimizer(), nesterov_optimizer(), rmsprop_optimizer(), adam_optimizer())
 
-lines1, lines2, lines3 = zip(*[(ax1.plot(z, label=l)[0], ax2.plot(x, y, 'o', label=l)[0], ax3.plot(x, y, z, 'o', label=l)[0]) 
-    for l, x, y, z in zip(labels, X, Y, Z)])
+lines1, lines2, lines3 = zip(*[(*ax1.plot(z), *ax2.plot(x, y, 'o'), *ax3.plot(x, y, z, 'o')) for x, y, z in zip(X, Y, Z)])
 
-ax1.legend()
+ax1.legend(labels)
 
-ani = animation.FuncAnimation(fig=fig, func=update, frames=epochs, interval=50, blit=True)
+ani = animation.FuncAnimation(fig=fig, func=update, frames=num, interval=50, blit=True)
 plt.show()
